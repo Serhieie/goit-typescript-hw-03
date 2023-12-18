3ab56782bf1ff5dad7efc745a439b0fc3ae78bb3
Serhieie <89160714+Serhieie@users.noreply.github.com>
2023-12-18 12:42:16 +0200

    OOP homework

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,34 @@
-const key = new Key();
+class Key {}
 
+class MyHouse {
+  constructor(private key: Key) {}
+
+  openDoor(personKey: Key) {
+    if (this.key === personKey) {
+      console.log("The door is unlocked. You can come in!");
+    } else {
+      console.log("You do not have the correct key to open the door.");
+    }
+  }
+
+  comeIn(person: Person) {
+    console.log(`${person.name} entered the house.`);
+  }
+}
+
+class Person {
+  constructor(private key: Key, public name: string) {}
+
+  getKey() {
+    return this.key;
+  }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
-const person = new Person(key);
+const person = new Person(key, "John");
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
 
 export {};
